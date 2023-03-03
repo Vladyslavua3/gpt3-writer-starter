5c8dc5b1aedc66b746561c18514b90091c15267a
Vladyslavua <vladgershman@gmail.com>
2023-03-03 20:25:12 +0100

    init:create nextjs app with gpt3 api

---
+++ pages/api/generate.js
@@ -0,0 +1,30 @@
+import { Configuration, OpenAIApi } from 'openai';
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+const basePromptPrefix = `Write me a Twitter thread post with the 5 most reasons about cryptocurrency which has written in title. Please ensure the blog post goes in-depth on the topic and shows that the writer did their research.
+Title:`
+
+
+const generateAction = async (req, res) => {
+    // Run first prompt
+    console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+    const baseCompletion = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `${basePromptPrefix}${req.body.userInput}`,
+        temperature: 0.8,
+        max_tokens: 250,
+    });
+
+    const basePromptOutput = baseCompletion.data.choices.pop();
+
+    res.status(200).json({ output: basePromptOutput });
+};
+
+export default generateAction;
